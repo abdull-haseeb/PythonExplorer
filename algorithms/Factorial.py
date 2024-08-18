4ff96eefda20c090e0e9c770da27a5f39678b423
Abdull Haseeb <121640819+abdull-haseeb@users.noreply.github.com>
2024-08-18 13:44:17 +0000

    factorial

--- algorithms/Factorial.py
+++ algorithms/Factorial.py
@@ -1,12 +1,8 @@
 def factorial(n, depth=0):
-    indent = " " * (depth * 4)  # Create indentation for better readability
-
+    indent = " " * (depth * 4)  
     if n == 1:
         return 1
     else:
-        # Recursive call
         result = factorial(n - 1, depth + 1)
         result = n * result
         return result
-
-# Test the function
