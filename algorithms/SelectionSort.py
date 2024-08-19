3b64711cc1997d5ec0d5b25d57076b538d1c0a23
Abdull Haseeb <121640819+abdull-haseeb@users.noreply.github.com>
2024-08-19 04:30:21 +0000

    selectionsort

--- algorithms/SelectionSort.py
+++ algorithms/SelectionSort.py
@@ -1,4 +1,14 @@
-def countdown(i):
-    return i - 1
+def selection_sort(arr):
+    for i in range(len(arr)):
+        min_index = i
+        for j in range(i+1, len(arr)):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+    
+    return arr
 
-print(countdown(5))  # Pass a single integer, not a list
+arr = [29, 10, 14, 37, 13]
+sorted_arr = selection_sort(arr)
+print(sorted_arr)  
